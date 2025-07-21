343047cc49134994bfd24ef1c545ee58690f2bf6
Katta Praveen Kumar <praveenkumar5500u@gmail.com>
2025-07-21 20:16:36 +0530

    Updated url

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ const app=express()
 app.use(express.json())
 
 app.use(cors({
-  origin: '*'
+  origin: ['http://localhost:5173', 'https://buymart.onrender.com'],
+  credentials: true
 }));
+
 
 
 connectDB()
