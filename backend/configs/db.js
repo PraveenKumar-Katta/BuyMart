a13a0687da83be77ca5fa8abd259d315f1a4a423
Katta Praveen Kumar <praveenkumar5500u@gmail.com>
2025-10-27 20:10:55 +0530

    bugs fixed

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,10 +1,8 @@
 const mongoose=require("mongoose")
-let productionURL=process.env.MONGO_URI
 
 const connectDB=async(req,res)=>{
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/Buymart")
-        console.log("DB Connected")
+        await mongoose.connect(process.env.MONGO_URI)
     } catch (error) {
         console.log("Error While Connecting Db",error.message)
     }
